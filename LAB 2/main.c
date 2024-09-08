ade3541aad7d1e3f417a51163f201fbd0ab75b87
MarcosVieira71 <marcosvieira120404@gmail.com>
2024-09-08 02:30:27 -0300

    Lab 2 remoção

--- LAB 2/main.c
+++ LAB 2/main.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,44 +17,67 @@ Patient* createPatient(int arrival, char* color);
 int searchByColor(char* color, Patient* head);
 void showPatients(Patient* head);
 
+Patient* removePatient(Patient* head, Patient* p)
+{
+ 	Patient* prev = NULL;
+    Patient* cur = head;
 
-Patient* insertPatient(Patient* head, Patient* p){
-	p->next = head;
-	return p;
+    for (;cur != NULL; cur=cur->next) {
+        if(cur->arrival == p->arrival) break;;
+		prev = cur;
+    }
+
+    if (cur) {
+        prev->next = cur->next;
+        free(cur);
+    }
+
+    return head;
+}
+
+Patient* insertPatient(Patient* head, Patient* p) {
+	Patient* cur = head;
+	Patient* prev = NULL;
+
+	for (cur; cur != NULL; cur = cur->next) {
+		if (p->color[0] == 'r' &&(cur->color[0] == 'y' || cur->color[0] == 'g')) {
+			break;
+		}
+		else if (p->color[0] == 'y' && cur->color[0] == 'g') {
+			break;
+		}
+		
+		prev = cur;
+	}
+
+	p->next = cur;
+
+	if (prev) {
+		prev->next = p;
+	}
+	else {
+		head = p;
+	}
+
+	return head;
 }
 
 
-int main(void){
+int main(void) {
 	FILE* file = open("arquivo.txt");
 	int tempInt;
 	char tempStr[10];
 	Patient* head = NULL;
 	while (fscanf(file, " %d %[^\n]", &tempInt, tempStr) == 2) {
 		Patient* p = createPatient(tempInt, tempStr);
-		if(!head){
-			head = insertPatient(head, p);
-			printf("Cai apenas uma vez\n");
-		}
-		else{
-			Patient* cur = head;
-			while(1){
-				Patient* next = cur->next;
-				if(!(head->next)){
-					head->next = insertPatient(head->next, p);
-					break;
-				}
-				if(p->color[0] == 'r'){
-					if(next->color[0] == 'y' || next->color[0] == 'g'){
-						p->next = cur->next;
-						cur->next = p;
-						break;		
-					}
-				}
-				cur = cur->next;
-			}
-		}
+		head = insertPatient(head, p);
+		
 	}
+	Patient* p = createPatient(12, "red");
+	head = removePatient(head, p);
+
 	showPatients(head);
+
 	return 0;
 }
 
@@ -68,23 +92,23 @@ Patient* createPatient(int arrival, char* color) {
 }
 
 int searchByColor(char* color, Patient* head) {
-	for (Patient* i = head; i != NULL; i=i->next) {
+	for (Patient* i = head; i != NULL; i = i->next) {
 		if (!strcmp(i->color, color)) return 1;
 	}
 	return 0;
 }
 
-FILE* open(char* filepath){
+FILE* open(char* filepath) {
 	FILE* p = fopen(filepath, "r");
-	if (p == NULL){
-	 exit(1);
-	 return NULL;
-	}	
+	if (p == NULL) {
+		exit(1);
+		return NULL;
+	}
 	return p;
 }
 
-void showPatients(Patient* head){
-	for(Patient* i = head; i != NULL; i=i->next){
+void showPatients(Patient* head) {
+	for (Patient* i = head; i != NULL; i = i->next) {
 		printf("%d %s\n", i->arrival, i->color);
 	}
 }
