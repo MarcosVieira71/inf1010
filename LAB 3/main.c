8950a3cb570bc5ef5cb6bd7322ac13032c69244e
juliazib <julia.zibordi@gmail.com>
2024-09-16 21:24:27 -0300

    comentarios

--- LAB 3/main.c
+++ LAB 3/main.c
@@ -3,54 +3,67 @@
 #include <stdlib.h>
 
 typedef struct arvore Arvore;
-struct arvore {
+struct arvore
+{
 	int chave;
-	Arvore* dir;
-	Arvore* esq;
+	Arvore *dir;
+	Arvore *esq;
 };
 
-Arvore* criaArvore(int chave);
-void insereArvoreNivel(Arvore** raiz, int chave);
-void exibeArvore(Arvore* raiz);
+Arvore *criaArvore(int chave);
+void insereArvoreNivel(Arvore **raiz, int chave);
+void exibeArvore(Arvore *raiz);
 
-int main(void) {
-	int arr[] = { 10,5,15,3,7,13,20,1,4,6 };
-	Arvore* raiz = NULL;
-	for (int i = 0; i < 10; i++) {
+int main(void)
+{
+	int arr[] = {10, 5, 15, 3, 7, 13, 20, 1, 4, 6};
+	Arvore *raiz = NULL;
+	for (int i = 0; i < 10; i++)
+	{
 		insereArvoreNivel(&raiz, arr[i]);
 	}
+	printf("\nExibindo arvore em pre ordem:\n");
 	exibeArvore(raiz);
 }
 
-void insereArvoreNivel(Arvore** raiz, int chave) {
-	if(*raiz == NULL){
+void insereArvoreNivel(Arvore **raiz, int chave)
+{
+	if (*raiz == NULL)
+	{
 		*raiz = criaArvore(chave);
 		return;
 	}
-	else{
-		Arvore** fila = (Arvore**)malloc(sizeof(Arvore*)*10);
+	else
+	{
+		// Aloca memória para a fila que será usada na busca por nível
+		Arvore **fila = (Arvore **)malloc(sizeof(Arvore *) * 10);
 		int inicio = 0, final = 0;
 		fila[final] = *raiz;
 		final++;
-		while(inicio < final){
-			Arvore* arvAtual = fila[inicio];
+		while (inicio < final)
+		{
+			Arvore *arvAtual = fila[inicio];
 			inicio++;
-			if(!arvAtual->esq){
+			// Se o filho esquerdo está vazio, insere o novo nó aqui
+			if (!arvAtual->esq)
+			{
 				arvAtual->esq = criaArvore(chave);
-				free(fila);
-				return;
+				break;
 			}
-			else{
-				fila[final] = arvAtual->esq;
+			else
+			{
+				fila[final] = arvAtual->esq; // Adiciona o filho esquerdo à fila
 				final++;
 			}
-			if(!arvAtual->dir){
+			// Se o filho direito está vazio, insere o novo nó aqui
+			if (!arvAtual->dir)
+			{
 				arvAtual->dir = criaArvore(chave);
-				free(fila);
-				return;
+				break;
 			}
-			else{
-				fila[final] = arvAtual->dir;
+			else
+			{
+				fila[final] = arvAtual->dir; // Adiciona o filho direito à fila
 				final++;
 			}
 		}
@@ -58,21 +71,25 @@ void insereArvoreNivel(Arvore** raiz, int chave) {
 	}
 }
 
-Arvore* criaArvore(int chave) {
-	Arvore* a = (Arvore*)malloc(sizeof(Arvore));
-	if (a == NULL) return NULL;
+Arvore *criaArvore(int chave)
+{
+	Arvore *a = (Arvore *)malloc(sizeof(Arvore));
+	if (a == NULL)
+		return NULL;
 	a->chave = chave;
 	a->dir = NULL;
 	a->esq = NULL;
 	return a;
 }
 
-
-void exibeArvore(Arvore* raiz) {
-	if (raiz == NULL) {
+void exibeArvore(Arvore *raiz)
+{
+	if (raiz == NULL)
+	{
 		return;
 	}
-	printf("%d ", raiz->chave);
+
 	exibeArvore(raiz->esq);
+	printf("%d\n", raiz->chave);
 	exibeArvore(raiz->dir);
 }
