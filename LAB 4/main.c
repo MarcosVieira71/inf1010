7a1ed9d2d2a69ca8c05a9c03ca6f1fabf4d3eeaa
juliazib <julia.zibordi@gmail.com>
2024-09-29 11:06:41 -0300

    adicionei altura e verificacao de arvore avl

--- LAB 4/main.c
+++ LAB 4/main.c
@@ -2,81 +2,104 @@
 #include <stdlib.h>
 
 typedef struct node Node;
-struct node{
-    Node* pai;
+struct node
+{
+    Node *pai;
     int chave;
     int numSub;
-    Node* dir;
-    Node* esq;
+    int altura;
+    Node *dir;
+    Node *esq;
 };
 
-Node* insereNo(Node* raiz, int chave);
-Node* criaNo(int chave);
-void exibeArvore(Node* raiz);
-int verificaAbb(Node* raiz);
-int contaSubNos(Node* raiz);
-int verificaAbb(Node* raiz);
-void preencheVetor(Node* raiz, int* arr, int* index);
-void liberaArvore(Node* raiz);
+Node *insereNo(Node *raiz, int chave);
+Node *criaNo(int chave);
+void exibeArvore(Node *raiz);
+int verificaAbb(Node *raiz);
+int contaSubNos(Node *raiz);
+int verificaAbb(Node *raiz);
+void preencheVetor(Node *raiz, int *arr, int *index);
+void liberaArvore(Node *raiz);
+int atualizaAltura(Node *raiz);
+int verificaAVL(Node *raiz);
 
-int main(void){
-    int arr[] = {15,17,3,5,2,20,25,13,10,16};
-    Node* raiz = NULL;
-    for(int i = 0; i < 10; i++){
+int main(void)
+{
+    int arr[] = {15, 17, 3, 5, 2, 20, 25, 13, 10, 16};
+    Node *raiz = NULL;
+    for (int i = 0; i < 10; i++)
+    {
         raiz = insereNo(raiz, arr[i]);
     }
+    atualizaAltura(raiz);
     printf("\nExibicao antes da contagem de sub-nos\n");
     exibeArvore(raiz);
     contaSubNos(raiz);
     printf("\nExibicao apos a contagem de sub-nos\n");
     exibeArvore(raiz);
-    if(verificaAbb(raiz)) printf("\nABB!\n");
+    if (verificaAbb(raiz))
+        printf("\nABB!\n");
+    if (verificaAVL(raiz))
+        printf("AVL\n");
     liberaArvore(raiz);
 }
 
-void exibeArvore(Node* raiz){
-    if(!raiz) return;
+void exibeArvore(Node *raiz)
+{
+    if (!raiz)
+        return;
     exibeArvore(raiz->esq);
-    printf("Raiz %p - Chave %d - NumSubs %d - Filho Dir %p - Filho Esq %p\n",raiz, raiz->chave, raiz->numSub, raiz->dir, raiz->esq);
+    printf("Raiz %p - Chave %d - NumSubs %d - Filho Dir %p - Filho Esq %p - Altura %d\n", raiz, raiz->chave, raiz->numSub, raiz->dir, raiz->esq, raiz->altura);
     exibeArvore(raiz->dir);
 }
 
-Node* insereNo(Node* raiz, int chave){
-    if(raiz == NULL){
+Node *insereNo(Node *raiz, int chave)
+{
+    if (raiz == NULL)
+    {
         return criaNo(chave);
     }
-    if(raiz->chave > chave){
+    if (raiz->chave > chave)
+    {
         raiz->esq = insereNo(raiz->esq, chave);
     }
 
-    else if(raiz->chave < chave){
+    else if (raiz->chave < chave)
+    {
         raiz->dir = insereNo(raiz->dir, chave);
     }
     return raiz;
 }
 
-Node* criaNo(int chave){
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    if(!newNode) exit(1);
+Node *criaNo(int chave)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode)
+        exit(1);
     newNode->chave = chave;
     newNode->pai = NULL;
     newNode->dir = NULL;
     newNode->esq = NULL;
     newNode->numSub = 0;
+    newNode->altura = 0;
     return newNode;
 }
 
-int verificaAbb(Node* raiz){
-    if(!raiz) return 0;
+int verificaAbb(Node *raiz)
+{
+    if (!raiz)
+        return 0;
     int numNos = contaSubNos(raiz);
 
-    int* arr = (int*)malloc(sizeof(int) * numNos);
-    
+    int *arr = (int *)malloc(sizeof(int) * numNos);
+
     int index = 0;
 
     preencheVetor(raiz, arr, &index);
-    for(int i = 0; i < numNos - 1; i++){
-        if(arr[i] > arr[i+1]){
+    for (int i = 0; i < numNos - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
             free(arr);
             return 0;
         }
@@ -85,28 +108,62 @@ int verificaAbb(Node* raiz){
     return 1;
 }
 
-int contaSubNos(Node* raiz) {
-    if (!raiz) return 0;
+int contaSubNos(Node *raiz)
+{
+    if (!raiz)
+        return 0;
     int qtdEsq = contaSubNos(raiz->esq);
     int qtdDir = contaSubNos(raiz->dir);
     raiz->numSub = qtdEsq + qtdDir;
     return raiz->numSub + 1;
 }
 
-void preencheVetor(Node* raiz, int* arr, int* index) {
-    if (raiz == NULL) return;
+void preencheVetor(Node *raiz, int *arr, int *index)
+{
+    if (raiz == NULL)
+        return;
 
     preencheVetor(raiz->esq, arr, index);
 
     arr[*index] = raiz->chave;
-    (*index)++;  
+    (*index)++;
 
     preencheVetor(raiz->dir, arr, index);
 }
 
-void liberaArvore(Node* raiz){
-    if(!raiz) return;
+void liberaArvore(Node *raiz)
+{
+    if (!raiz)
+        return;
     liberaArvore(raiz->esq);
     liberaArvore(raiz->dir);
     free(raiz);
+}
+
+int atualizaAltura(Node *raiz)
+{
+    if (!raiz)
+        return -1;
+    int alturaesq = atualizaAltura(raiz->esq);
+    int alturadir = atualizaAltura(raiz->dir);
+    raiz->altura = ((alturaesq > alturadir) ? alturaesq : alturadir) + 1;
+
+    return raiz->altura;
+}
+
+int verificaAVL(Node *raiz)
+{
+    if (!raiz)
+        return 1;
+
+    if (!verificaAbb(raiz))
+        return 0;
+
+    int alturaesq = raiz->esq ? raiz->esq->altura : -1;
+    int alturadir = raiz->dir ? raiz->dir->altura : -1;
+
+    if (abs(alturaesq - alturadir) > 1)
+        return 0;
+
+    return verificaAVL(raiz->esq) && verificaAVL(raiz->dir);
 }
