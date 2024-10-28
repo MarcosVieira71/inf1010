c261c681702ba2aa24c7116d7062244f5ece317a
Marcos <marcosvieira@tecgraf.puc-rio.br>
2024-10-28 18:29:28 -0300

    fix

--- T1/main.c
+++ T1/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TAMANHO 1321
+#define TAMANHO 1249
 
 char** inicializaHashTable() {
     char** hashTable = (char**)malloc(sizeof(char*) * TAMANHO);
