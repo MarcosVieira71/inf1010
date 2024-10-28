def60fdf68f4c1e4ee86488c39a890eec06780ea
juliazib <julia.zibordi@gmail.com>
2024-10-27 21:07:56 -0300

    libera hash table e adiciona func secundaria de hashing para tratar colisoes

--- T1/main.c
+++ T1/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TAMANHO 20
+#define TAMANHO 29
 
 char** inicializaHashTable(){
     char** hashTable = (char**)malloc(sizeof(char*) * TAMANHO);
@@ -15,9 +15,17 @@ char** inicializaHashTable(){
 int hashCPF(char* cpf){
     unsigned int hash = 0;
     for(int i = 0; cpf[i] != '\0'; i++){
-        hash = (hash * 31) + cpf[i] - '0';
+        hash = (hash * 31) ^ (cpf[i] - '0');
     }
     return hash % TAMANHO;
+}
+
+int hashSecundaria(char* cpf) {
+    unsigned int hash = 0;
+     for (int i = 0; cpf[i] != '\0'; i++) {
+        hash = (hash * 37) + (cpf[i] - '0');  
+    }
+    return 1 + (hash % (TAMANHO - 1)); 
 }
 
 int main(void){
@@ -31,19 +39,29 @@ int main(void){
     char cpf[12];
     int cont = 0;
     int colisoes = 0;
+
     while (fscanf(arquivoCpfs, "%11s", cpf) != EOF && cont < 20){ 
         int idx = hashCPF(cpf);
-        if(!hashTable[idx]){
-            hashTable[idx] = strdup(cpf);
+
+        while(hashTable[idx] != NULL) {
+            colisoes++;
+            idx = (idx + hashSecundaria(cpf))% TAMANHO;
         }
-        else colisoes++;
+        
+        hashTable[idx] = strdup(cpf);
         cont++;
     }
 
-    for(int i = 0; i <= 20; i++){
-        printf("%dº CPF armazenadao = %s\n",i,  hashTable[i]);
+    for(int i = 0; i < TAMANHO; i++){
+        printf("%dº CPF armazenado = %s\n",i,  hashTable[i]);
     }
 
     printf("Colisões: %d\n", colisoes);
     fclose(arquivoCpfs);
+
+    for(int i = 0; i < TAMANHO; i++) {
+        free(hashTable[i]);
+    }
+
+    free(hashTable);
 }
